dba415a3281b0aa981437652aa354611930a8802
xmlscript <xmlscript@gmail.com>
2018-04-25 22:48:57 +0800

    dev:

--- invoke.php
+++ invoke.php
@@ -46,13 +46,9 @@ class invoke{
     $file= $this->dir.DIRECTORY_SEPARATOR.'.'.$this->appid.'.'.$filename;
     $iv = substr($this->appid,-16);
 
-
     file_put_contents($file, openssl_encrypt($result->access_token,self::CIPHER,$this->secret,OPENSSL_RAW_DATA,$iv)) or error_log("无法写入$file");
 
-
-    throw new \Error($file.' '.function_exists('openssl_encrypt')?'y':'n'.' '.OPENSSL_RAW_DATA);
-
-    //throw new \Error($this->load($filename,7200));
+    throw new \Error($file.' '.$this->load($filename,7200));
 
     return $str;
 
