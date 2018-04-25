7666183e75098421c8bd1391ee6f2f58d769413d
xmlscript <xmlscript@gmail.com>
2018-04-25 21:40:56 +0800

    dev

--- invoke.php
+++ invoke.php
@@ -46,9 +46,7 @@ class invoke{
     $file= $this->dir.DIRECTORY_SEPARATOR.'.'.$this->appid.'.'.$filename;
     $iv = substr($this->appid,-16);
 
-    file_put_contents($file, openssl_encrypt($result->access_token,self::CIPHER,$this->secret,OPENSSL_RAW_DATA,$iv))
-      or error_log("无法写入$file")
-      and throw new \Error($file.' '.openssl_encrypt($result->access_token,self::CIPHER,$this->secret,OPENSSL_RAW_DATA,$iv));
+    file_put_contents($file, openssl_encrypt($result->access_token,self::CIPHER,$this->secret,OPENSSL_RAW_DATA,$iv)) or error_log("无法写入$file");
 
     throw new \Error($file.' '.openssl_encrypt($result->access_token,self::CIPHER,$this->secret,OPENSSL_RAW_DATA,$iv));
 
