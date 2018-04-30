aff8b2ab595708e3e9f5c457db9533a7c666c7fa
xmlscript <xmlscript@gmail.com>
2018-04-30 17:10:05 +0800

    extract token

--- token.php
+++ token.php
@@ -1,11 +1,29 @@
-<?php namespace mp;
+<?php namespace mp; // vim: se fdm=marker:
 
-class token{
-  function __construct(){
+use http\request;
+use tmp\cache;
 
+final class token{
+
+  private const HOST = 'https://api.weixin.qq.com';
+  private $appid,$secret,$expires_in=7200;
+  
+  function __construct(string $appid, string $secret){
+    $this->appid = $appid;
+    $this->secret = $secret;
   }
 
-  function __toString(){
-
+  function __toString():string{
+    return new cache($this->appid.__CLASS__, $this->secret, $this->expires_in, function(){
+      $result = request::url(self::HOST.'/cgi-bin/token')
+        ->fetch(['grant_type'=>'client_credential','appid'=>$this->appid,'secret'=>$this->secret])
+        ->json();
+      if(isset($result->access_token)){
+        $this->expires_in = $result->expires_in;
+        return $result->access_token;
+      }else
+        error_log($result->errmsg);
+    });
   }
+
 }
