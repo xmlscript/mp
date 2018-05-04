169e20112e991a9548c135a4d994804d94f57f27
xmlscript <xmlscript@gmail.com>
2018-05-04 18:43:15 +0800

    dev

--- quote.php
+++ quote.php
@@ -9,18 +9,20 @@ class quote{
   final function __construct(string $token, string $host='https://api.weixin.qq.com'){
     $this->token = $token;
   }
+
+  private function check(\stdClass $json):\stdClass{
+    if(isset($json->errcode,$json->errmsg)&&$json->errcode)
+      throw new \RuntimeException($json->errmsg,$json->errcode);
+    return $json;
+  }
   
   
-  /**
-   * 获取或重置调用次数限额，微信公众号平台限制每个接口不同的调用次数，超过后，每月允许重置n次
-   * @todo api需要额外限制不能频繁调用，否则导致月底不可访问
-   * @todo 忘记了，先看看能不能查询限额
-   * @see 
-   */
-  function quota():bool{
-    $response = request::get($this->host.'/cgi-bin/xxxxxxxxxxxxxxxx')
+  function clear(string $appid):bool{
+    https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1433744592
+    return $appid&&$this->check(request::url(self::HOST.'/cgi-bin/clear_quota')
       ->query(['access_token'=>$this->token])
-      ->POST();
+      ->POST(json_encode(['appid'=>$appid]))
+      ->body());
   }
 
 }
