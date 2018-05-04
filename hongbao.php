fd3456c0b92c6db2ad1bfcfdea941ca44f351baf
xmlscript <xmlscript@gmail.com>
2018-05-04 19:01:46 +0800

    dev: 摇一摇及其包含的红包等子功能太繁琐了！！！

--- hongbao.php
+++ hongbao.php
@@ -10,13 +10,22 @@ class hongbao{
     $this->token = $token;
   }
 
+  private function check(\SimpleXMLElement $xml):\SimpleXMLElement{
+    if(isset($xml->return_code,$xml->return_msg)&&$xml->return_code==='FAIL')
+      throw new \RuntimeException($xml->return_msg,$xml->return_code);
+    return $xml;
+  }
+
 
   /**
    * 设置单个红包的金额，类型等，生成红包信息。预下单完成后，需要在72小时内调用jsapi完成抽红包的操作。（红包过期失效后，资金会退回到商户财付通帐号。）
    */
   function 发红包(){
     https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1459327193
-    $response = request::url('https://api.mch.weixin.qq.com/mmpaymkttransfers/hbpreorder')->POST();
+    return $this->check(request::url('https://api.mch.weixin.qq.com/mmpaymkttransfers/hbpreorder')
+      ->header('Content-Type','application/xml')
+      ->POST()
+      ->xml());
   }
 
   function 创建红包(){
